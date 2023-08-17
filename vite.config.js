8c820055098e1c6a437ae7d9020ec10bc383b44b
Mario Perez <mario.perez415@gmail.com>
2023-08-17 10:24:20 -0400

    Update vite workflow for working with remote site

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,50 @@
-import { defineConfig } from "vite";
 import path from "path";
-import basicSsl from "@vitejs/plugin-basic-ssl";
+import { defineConfig, loadEnv } from "vite";
+import mkcert from "vite-plugin-mkcert";
 
-export default defineConfig({
-    resolve: {
-        alias: {
-            "@": path.resolve(__dirname, "./"),
+// vite.config.js
+export default defineConfig((command, mode) => {
+    const env = loadEnv(mode, process.cwd(), "");
+    const baseUrl =
+        env.NODE_ENV === "production"
+            ? env.PRODUCTION_URL
+            : "https://localhost:3000";
+    return {
+        base: baseUrl,
+        assetsInclude: ["**/*.gltf", "**/*.glb"],
+        resolve: {
+            alias: {
+                "@": path.resolve(__dirname, "./"),
+            },
         },
-    },
-    plugins: [basicSsl()],
+        server: {
+            https: true,
+            host: "localhost",
+            port: "3000",
+            cors: "*",
+            origin: "https://localhost:3000",
+            hmr: {
+                host: "localhost",
+                protocol: "wss",
+            },
+        },
+        plugins: [mkcert()],
+        build: {
+            minify: true,
+            manifest: true,
+            rollupOptions: {
+                input: "./js/main.js",
+                output: {
+                    format: "umd",
+                    entryFileNames: "main.js",
+                    esModule: false,
+                    compact: true,
+                    globals: {
+                        jquery: "$",
+                    },
+                },
+                external: ["jquery"],
+            },
+        },
+    };
 });
