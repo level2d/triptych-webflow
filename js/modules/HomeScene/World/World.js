0abb4af3c8b1a6c49d9f8e5757d47739ba215731
Mario Perez <mario.perez415@gmail.com>
2023-08-21 23:06:07 -0400

    Setup parallax group and updates for world

--- js/modules/HomeScene/World/World.js
+++ js/modules/HomeScene/World/World.js
@@ -1,16 +1,26 @@
 import * as THREE from "three";
 import HomeScene from "../HomeScene";
+import ParallaxGroup from "./ParallaxGroup";
 
 export default class World {
+    parallaxGroup = null;
+
     constructor() {
         this.homeScene = new HomeScene();
         this.scene = this.homeScene.scene;
+        this.resources = this.homeScene.resources;
+
+        this.parallaxGroup = new ParallaxGroup();
 
         // Test mesh
         const testMesh = new THREE.Mesh(
             new THREE.BoxGeometry(1, 1, 1),
             new THREE.MeshBasicMaterial({ wireframe: true })
         );
-        this.scene.add(testMesh);
+        this.parallaxGroup.group.add(testMesh);
+    }
+
+    update() {
+        this.parallaxGroup.update();
     }
 }
