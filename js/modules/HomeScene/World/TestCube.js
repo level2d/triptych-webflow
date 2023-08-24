d5dc52325fb2eb44d2eabbd915e556d4218fd00d
Mario Perez <mario.perez415@gmail.com>
2023-08-24 19:39:15 -0400

    Setup test cube click and zoom

--- js/modules/HomeScene/World/TestCube.js
+++ js/modules/HomeScene/World/TestCube.js
@@ -1,16 +1,29 @@
 import * as THREE from "three";
+import { dampC, dampE } from "maath/easing";
 
 import HomeScene from "../HomeScene";
 
 export default class TestCube {
     model = null;
+    items = [];
+    currentIntersects = [];
+    padding = 0.02; // padding to use for controls.fitToBox()
+
     constructor() {
         this.homeScene = new HomeScene();
+        this.debug = this.homeScene.debug;
+        this.raycaster = this.homeScene.raycaster;
+        this.camera = this.homeScene.camera;
+        this.cursor = this.homeScene.cursor;
+        this.time = this.homeScene.time;
         this.rootGroup = this.homeScene.world.rootGroup;
         this.resources = this.homeScene.resources;
 
         this.resource = this.resources.items.testCubeModel;
+        this.handleClick = this.handleClick.bind(this);
         this.setModel();
+        this.setItems();
+        this.bindListeners();
     }
 
     setModel() {
@@ -39,5 +52,89 @@ export default class TestCube {
         group.add(this.model);
     }
 
-    setItems() {}
+    setItems() {
+        const mesh1 = new THREE.Mesh(
+            new THREE.TorusKnotGeometry(0.05, 0.01, 64, 8),
+            new THREE.MeshStandardMaterial()
+        );
+        const mesh2 = new THREE.Mesh(
+            new THREE.TorusKnotGeometry(0.05, 0.01, 64, 8),
+            new THREE.MeshStandardMaterial()
+        );
+        const mesh3 = new THREE.Mesh(
+            new THREE.TorusKnotGeometry(0.05, 0.01, 64, 8),
+            new THREE.MeshStandardMaterial()
+        );
+
+        mesh1.name = "mesh1";
+        mesh2.name = "mesh2";
+        mesh3.name = "mesh3";
+        mesh1.position.set(0.37, -0.38, 0.4);
+        mesh2.position.set(0.12, 0.38, 0.4);
+        mesh3.position.set(-0.12, 0.12, 0.4);
+
+        this.rootGroup.group.add(mesh1, mesh2, mesh3);
+        this.items = [mesh1, mesh2, mesh3];
+    }
+
+    update() {
+        if (this.items.length <= 0) return;
+
+        const {
+            items,
+            raycaster,
+            camera: { instance: camera },
+            cursor,
+            time: { delta },
+        } = this;
+        const coords = new THREE.Vector2(cursor.x, cursor.y);
+
+        raycaster.setFromCamera(coords, camera);
+        this.currentIntersects = raycaster.intersectObjects(items);
+
+        items.forEach((item) => {
+            const x = -cursor.y * 0.5;
+            const y = item.rotation.y;
+            const z = -cursor.x * 0.5;
+            dampE(item.rotation, [x, y, z], 0.15, delta);
+        });
+
+        if (this.rotatableItem) {
+            this.rotatableItem.y += Math.PI * 0.01;
+        }
+    }
+
+    async handleClick() {
+        if (this.currentIntersects.length > 0) {
+            this.camera.controls.saveState(); // save camera position for zooming back out
+
+            const clickedObject = this.currentIntersects[0].object;
+            const targetPosition = clickedObject.getWorldPosition(
+                new THREE.Vector3()
+            );
+            const currentPosition = new THREE.Vector3();
+            this.camera.controls.getPosition(currentPosition);
+
+            await this.camera.controls.setLookAt(
+                targetPosition.x,
+                targetPosition.y,
+                currentPosition.z,
+                targetPosition.x,
+                targetPosition.y,
+                targetPosition.z,
+                true
+            );
+
+            await this.camera.controls.fitToBox(clickedObject, true, {
+                paddingTop: this.padding,
+                paddingRight: this.padding,
+                paddingBottom: this.padding,
+                paddingLeft: this.padding,
+            });
+        }
+    }
+
+    bindListeners() {
+        window.addEventListener("click", this.handleClick);
+    }
 }
