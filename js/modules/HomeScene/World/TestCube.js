89b4117a75439b110d8179e0d24edf5e73b1007a
Mario Perez <mario.perez415@gmail.com>
2023-08-24 21:39:27 -0400

    Remove parallax group

--- js/modules/HomeScene/World/TestCube.js
+++ js/modules/HomeScene/World/TestCube.js
@@ -17,7 +17,6 @@ export default class TestCube {
         this.cursor = this.homeScene.cursor;
         this.time = this.homeScene.time;
         this.rootGroup = this.homeScene.world.rootGroup;
-        this.parallaxGroup = this.homeScene.world.parallaxGroup;
         this.resources = this.homeScene.resources;
 
         this.resource = this.resources.items.testCubeModel;
