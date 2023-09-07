1b538ef90e55abdcb45bc4223d282527a0753413
Mario Perez <mario.perez415@gmail.com>
2023-09-07 01:23:44 -0400

    Setup cube

--- js/components/HomeScene/Scene/Cube.jsx
+++ js/components/HomeScene/Scene/Cube.jsx
@@ -1,18 +1,26 @@
+import { useRef } from "react";
 import { useGLTF, Outlines } from "@react-three/drei";
 import { constants } from "@/js/core";
 
 export default function Cube() {
+    const cubeRef = useRef(null);
     const model = useGLTF(constants.GLB_ASSET_URLS.Test_Cube);
+    console.log(model);
     return (
-        <group>
-            <mesh
-                geometry={model.nodes.cube.geometry}
-                material={model.nodes.cube.material}
-                scale={0.2}
-            >
-                <Outlines angle={0} thickness={0.1} color="black" />
-            </mesh>
-        </group>
+        <>
+            <group>
+                <mesh
+                    castShadow={true}
+                    receiveShadow={true}
+                    geometry={model.nodes.cube.geometry}
+                    material={model.nodes.cube.material}
+                    scale={0.2}
+                    ref={cubeRef}
+                >
+                    <Outlines angle={0} thickness={0.1} color="black" />
+                </mesh>
+            </group>
+        </>
     );
 }
 
