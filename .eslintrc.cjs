58d9a9f735022bd98d6f867523836d0668bdb97b
Mario Perez <mario.perez415@gmail.com>
2023-09-06 22:40:56 -0400

    Fix lintint. Add prettier

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -7,12 +7,14 @@ module.exports = {
         "plugin:react/jsx-runtime",
         "plugin:react-hooks/recommended",
         "plugin:@react-three/recommended",
+        "eslint-config-prettier",
     ],
     ignorePatterns: ["dist", ".eslintrc.cjs", "vit.config.js"],
     parserOptions: { ecmaVersion: "latest", sourceType: "module" },
-    settings: { react: { version: "18.2" } },
+    settings: { react: { version: "detect" } },
     plugins: ["react-refresh"],
     rules: {
+        "react/no-unknown-property": ["off"], // turn off b/c r3f introduces custom properties
         "react-refresh/only-export-components": [
             "warn",
             { allowConstantExport: true },
