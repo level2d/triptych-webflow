bf51feb9408dccb1d4e73b7e72e72109c95b18c5
Mario Perez <mario.perez415@gmail.com>
2023-09-07 01:23:37 -0400

    Setup scene context. Add camera controls to context

--- js/components/HomeScene/Scene/Scene.jsx
+++ js/components/HomeScene/Scene/Scene.jsx
@@ -1,23 +1,65 @@
-import { OrbitControls, Environment } from "@react-three/drei";
+import { useEffect, useRef, useState } from "react";
+import { useThree } from "@react-three/fiber";
+import {
+    CameraControls,
+    Environment,
+    OrthographicCamera,
+    OrbitControls,
+} from "@react-three/drei";
 import { Perf } from "r3f-perf";
 
 import { debug } from "@/js/core/constants";
+import { SceneContext } from "./SceneContext";
 import Cube from "./Cube";
 
+const padding = 0.5;
+
 export default function Scene() {
+    const [mounted, setMounted] = useState(false);
+    const { size } = useThree();
+    const { width, height } = size;
+    const cameraControls = useRef(null);
+    const groupRef = useRef(null);
+
+    useEffect(() => {
+        setMounted(true);
+    }, []);
+
+    useEffect(() => {
+        if (!cameraControls.current) return;
+        if (mounted) {
+            cameraControls.current.fitToBox(groupRef.current, true, {
+                paddingTop: padding,
+                paddingRight: padding,
+                paddingBottom: padding,
+                paddingLeft: padding,
+            });
+        }
+    }, [mounted, width, height, cameraControls]);
+
     return (
-        <>
-            {/* controls */}
+        <SceneContext.Provider value={{ cameraControls }}>
+            {/* debug */}
             {debug && <Perf position="top-left" />}
-            <OrbitControls makeDefault />
+            {debug && <OrbitControls />}
+
+            {/* camera */}
+            <OrthographicCamera makeDefault zoom={200} position={[0, 0, 10]} />
+            <CameraControls ref={cameraControls} />
 
             {/* environment */}
             <Environment preset="city" background blur={1} />
-            <directionalLight castShadow position={[1, 2, 3]} intensity={1.5} />
+            <directionalLight
+                castShadow
+                position={[-1.5, 2.5, 1.8]}
+                intensity={1.5}
+            />
             <ambientLight intensity={0.5} />
 
             {/* models */}
-            <Cube />
+            <group ref={groupRef}>
+                <Cube />
+            </group>
 
             {/* Plane */}
             <mesh
@@ -29,6 +71,6 @@ export default function Scene() {
                 <planeGeometry />
                 <meshStandardMaterial color="greenyellow" />
             </mesh>
-        </>
+        </SceneContext.Provider>
     );
 }
