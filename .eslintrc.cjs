df22ff8aac8e9bff8113bec24286cc4a16cbe36d
Mario Perez <mario.perez415@gmail.com>
2023-09-06 21:53:36 -0400

    Add @react-three eslint plugin

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -6,6 +6,7 @@ module.exports = {
         "plugin:react/recommended",
         "plugin:react/jsx-runtime",
         "plugin:react-hooks/recommended",
+        "plugin:@react-three/recommended",
     ],
     ignorePatterns: ["dist", ".eslintrc.cjs", "vit.config.js"],
     parserOptions: { ecmaVersion: "latest", sourceType: "module" },
