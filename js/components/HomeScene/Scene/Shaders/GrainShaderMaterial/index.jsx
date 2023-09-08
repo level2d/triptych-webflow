0f4f80cb31bef376b08c1c7e8bfda595f6de939a
Mario Perez <mario.perez415@gmail.com>
2023-09-08 16:40:29 -0400

    Use imports from glsl files

--- js/components/HomeScene/Scene/Shaders/GrainShaderMaterial/index.jsx
+++ js/components/HomeScene/Scene/Shaders/GrainShaderMaterial/index.jsx
@@ -1,16 +1,28 @@
+import * as THREE from "three";
 import { extend } from "@react-three/fiber";
 import { shaderMaterial } from "@react-three/drei";
 
 import vertexShaderCode from "./vertex.glsl";
 import fragmentShaderCode from "./fragment.glsl";
+import { constants } from "@/js/core";
+
+const textureLoader = new THREE.TextureLoader();
+const matcapTexture = await textureLoader.loadAsync(
+    constants.TEXTURE_ASSET_URLS.rim_lit_matcap,
+);
 
 export const GrainShaderMaterial = shaderMaterial(
     // Uniform
-    { uHeight: 10.0, uPatternScale: { x: 0.1, y: 0.1 } },
+    {
+        uBoundingBoxMin: new THREE.Vector3(0, 0, 0),
+        uBoundingBoxMax: new THREE.Vector3(1, 1, 1),
+        uPatternScale: { x: 0.1, y: 0.1 },
+        uMatcapTexture: matcapTexture,
+    },
     // Vertex Shader
-    vertexShaderCode,
+    `${vertexShaderCode}`,
     // Fragment Shader
-    fragmentShaderCode,
+    `${fragmentShaderCode}`,
 );
 
 extend({ GrainShaderMaterial });
