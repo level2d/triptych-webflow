410e54448ee20a7fd95e3cacc42d90702b0c6162
Mario Perez <mario.perez415@gmail.com>
2023-09-08 16:38:42 -0400

    Add mat cap textures

--- js/core/constants.js
+++ js/core/constants.js
@@ -2,8 +2,8 @@
 import box_locations_url from "@/assets/3d/box_locations_05.glb";
 
 // textures
-import cell_matcap_url from "@/assets/2d/cell_matcap_04.png";
-import running_man_url from "@/assets/2d/running_man_01.mp4?url";
+import hatching_matcap_url from "@/assets/img/hatching_matcap_02.png";
+import matcap_rim_lit_url from "@/assets/img/matcap_rim_lit.jpg";
 
 // shaders
 import matcap_shader_url from "@/assets/2d/matcap_02.json?url";
@@ -14,8 +14,8 @@ export const GLB_ASSET_URLS = {
 };
 
 export const TEXTURE_ASSET_URLS = {
-    matcap: cell_matcap_url,
-    running_man: running_man_url,
+    hatching_matcap: hatching_matcap_url,
+    rim_lit_matcap: matcap_rim_lit_url,
 };
 
 export const SHADER_ASSET_URLS = {
