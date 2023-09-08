c61de2a4f131fc6e5ad50b96ebe2aefbb2d5f054
Mario Perez <mario.perez415@gmail.com>
2023-09-08 11:18:56 -0400

    Improve loading. Prevent lenis

--- js/components/HomeScene/HomeScene.jsx
+++ js/components/HomeScene/HomeScene.jsx
@@ -2,7 +2,7 @@ import styles from "./HomeScene.module.scss";
 
 import { Suspense, lazy } from "react";
 import { Canvas } from "@react-three/fiber";
-import { Html } from "@react-three/drei";
+import { Html, Text } from "@react-three/drei";
 import { Leva } from "leva";
 import { debug } from "@/js/core/constants";
 
@@ -11,33 +11,37 @@ import loadingLogoUrl from "@/assets/img/loading-logo.gif";
 const Scene = lazy(() => {
     return Promise.all([
         import("./Scene"),
-        new Promise((resolve) => setTimeout(resolve, debug ? 0 : 2000)),
+        new Promise((resolve) => setTimeout(resolve, debug ? 0 : 2.4 * 1000)),
     ]).then(([moduleExports]) => moduleExports);
 });
 
+const Loading = () => {
+    return debug ? (
+        <Text position={[0, 0, 0]} rotation={[0, Math.PI * -0.25, 0]}>
+            {"Loading..."}
+        </Text>
+    ) : (
+        <Html
+            position={[0, 0, 0]}
+            style={{
+                height: "100%",
+                width: "100%",
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+            }}
+        >
+            <img src={loadingLogoUrl} />
+        </Html>
+    );
+};
+
 export default function HomeScene() {
     return (
-        <div className={styles.wrapper}>
+        <div className={styles.wrapper} data-lenis-prevent>
             <Leva hidden={!debug} />
             <Canvas shadows camera={{ position: [-4, -1.25, 4], fov: 55 }}>
-                <Suspense
-                    fallback={
-                        debug ? null : (
-                            <Html
-                                position={[0, 0, 0]}
-                                style={{
-                                    height: "100%",
-                                    width: "100%",
-                                    display: "flex",
-                                    justifyContent: "center",
-                                    alignItems: "center",
-                                }}
-                            >
-                                <img src={loadingLogoUrl} />
-                            </Html>
-                        )
-                    }
-                >
+                <Suspense fallback={<Loading />}>
                     <Scene />
                 </Suspense>
             </Canvas>
