ae6afec18d740648870fdf40539a07eff16927fe
Mario Perez <mario.perez415@gmail.com>
2023-09-09 10:07:17 -0400

    Setup placeholder torus knot meshes

--- js/components/HomeScene/Scene/Model.jsx
+++ js/components/HomeScene/Scene/Model.jsx
@@ -47,108 +47,168 @@ function _Model(props, ref) {
             <mesh
                 castShadow
                 receiveShadow
-                geometry={nodes.location_015.geometry}
-                material={nodes.location_015.material}
+                // geometry={nodes.location_015.geometry}
+                // material={nodes.location_015.material}
                 position={[1, 1, -3]}
-            />
+                scale={0.35}
+            >
+                <torusKnotGeometry />
+                <meshStandardMaterial color={"red"} />
+            </mesh>
             <mesh
                 castShadow
                 receiveShadow
-                geometry={nodes.location_001.geometry}
-                material={nodes.location_001.material}
+                // geometry={nodes.location_001.geometry}
+                // material={nodes.location_001.material}
                 position={[1, 3, -3]}
-            />
+                scale={0.35}
+            >
+                <torusKnotGeometry />
+                <meshStandardMaterial color={"red"} />
+            </mesh>
             <mesh
                 castShadow
                 receiveShadow
                 geometry={nodes.location_002.geometry}
                 material={nodes.location_002.material}
                 position={[-3, -1, -3]}
-            />
+                scale={0.35}
+            >
+                <torusKnotGeometry />
+                <meshStandardMaterial color={"red"} />
+            </mesh>
             <mesh
                 castShadow
                 receiveShadow
-                geometry={nodes.location_003.geometry}
-                material={nodes.location_003.material}
+                // geometry={nodes.location_003.geometry}
+                // material={nodes.location_003.material}
                 position={[-3, 3, 1]}
-            />
+                scale={0.35}
+            >
+                <torusKnotGeometry />
+                <meshStandardMaterial color={"red"} />
+            </mesh>
             <mesh
                 castShadow
                 receiveShadow
-                geometry={nodes.location_004.geometry}
-                material={nodes.location_004.material}
+                // geometry={nodes.location_004.geometry}
+                // material={nodes.location_004.material}
                 position={[-1, -1, 1]}
-            />
+                scale={0.35}
+            >
+                <torusKnotGeometry />
+                <meshStandardMaterial color={"red"} />
+            </mesh>
             <mesh
                 castShadow
                 receiveShadow
-                geometry={nodes.location_005.geometry}
-                material={nodes.location_005.material}
+                // geometry={nodes.location_005.geometry}
+                // material={nodes.location_005.material}
                 position={[3, -1, 3]}
-            />
+                scale={0.35}
+            >
+                <torusKnotGeometry />
+                <meshStandardMaterial color={"red"} />
+            </mesh>
             <mesh
                 castShadow
                 receiveShadow
-                geometry={nodes.location_006.geometry}
-                material={nodes.location_006.material}
+                // geometry={nodes.location_006.geometry}
+                // material={nodes.location_006.material}
                 position={[-1, -3, 1]}
-            />
+                scale={0.35}
+            >
+                <torusKnotGeometry />
+                <meshStandardMaterial color={"red"} />
+            </mesh>
             <mesh
                 castShadow
                 receiveShadow
-                geometry={nodes.location_007.geometry}
-                material={nodes.location_007.material}
+                // geometry={nodes.location_007.geometry}
+                // material={nodes.location_007.material}
                 position={[1, -1, -1]}
-            />
+                scale={0.35}
+            >
+                <torusKnotGeometry />
+                <meshStandardMaterial color={"red"} />
+            </mesh>
             <mesh
                 castShadow
                 receiveShadow
-                geometry={nodes.location_008.geometry}
-                material={nodes.location_008.material}
+                // geometry={nodes.location_008.geometry}
+                // material={nodes.location_008.material}
                 position={[-1, 3, 3]}
-            />
+                scale={0.35}
+            >
+                <torusKnotGeometry />
+                <meshStandardMaterial color={"red"} />
+            </mesh>
             <mesh
                 castShadow
                 receiveShadow
-                geometry={nodes.location_009.geometry}
-                material={nodes.location_009.material}
+                // geometry={nodes.location_009.geometry}
+                // material={nodes.location_009.material}
                 position={[-3, -3, -3]}
-            />
+                scale={0.35}
+            >
+                <torusKnotGeometry />
+                <meshStandardMaterial color={"red"} />
+            </mesh>
             <mesh
                 castShadow
                 receiveShadow
-                geometry={nodes.location_010.geometry}
-                material={nodes.location_010.material}
+                // geometry={nodes.location_010.geometry}
+                // material={nodes.location_010.material}
                 position={[3, 3, -1]}
-            />
+                scale={0.35}
+            >
+                <torusKnotGeometry />
+                <meshStandardMaterial color={"red"} />
+            </mesh>
             <mesh
                 castShadow
                 receiveShadow
-                geometry={nodes.location_011.geometry}
-                material={nodes.location_011.material}
+                // geometry={nodes.location_011.geometry}
+                // material={nodes.location_011.material}
                 position={[3, -3, -1]}
-            />
+                scale={0.35}
+            >
+                <torusKnotGeometry />
+                <meshStandardMaterial color={"red"} />
+            </mesh>
             <mesh
                 castShadow
                 receiveShadow
-                geometry={nodes.location_012.geometry}
-                material={nodes.location_012.material}
+                // geometry={nodes.location_012.geometry}
+                // material={nodes.location_012.material}
                 position={[1, -3, 1]}
-            />
+                scale={0.35}
+            >
+                <torusKnotGeometry />
+                <meshStandardMaterial color={"red"} />
+            </mesh>
             <mesh
                 castShadow
                 receiveShadow
-                geometry={nodes.location_013.geometry}
-                material={nodes.location_013.material}
+                // geometry={nodes.location_013.geometry}
+                // material={nodes.location_013.material}
                 position={[-1, 1, 1]}
-            />
+                scale={0.35}
+            >
+                <torusKnotGeometry />
+                <meshStandardMaterial color={"red"} />
+            </mesh>
             <mesh
                 castShadow
                 receiveShadow
-                geometry={nodes.location_014.geometry}
-                material={nodes.location_014.material}
+                // geometry={nodes.location_014.geometry}
+                // material={nodes.location_014.material}
                 position={[3, 1, 3]}
-            />
+                scale={0.35}
+            >
+                <torusKnotGeometry />
+                <meshStandardMaterial color={"red"} />
+            </mesh>
         </group>
     );
 }
