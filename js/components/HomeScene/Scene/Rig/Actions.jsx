562cf9a4bc606f224e0d96b3c93e3f766a756bad
Mario Perez <mario.perez415@gmail.com>
2023-09-18 22:34:12 -0400

    Setup keyboard events

--- js/components/HomeScene/Scene/Rig/Actions.jsx
+++ js/components/HomeScene/Scene/Rig/Actions.jsx
@@ -1,4 +1,5 @@
 import { useControls, button } from "leva";
+import { useHotkeys } from "react-hotkeys-hook";
 
 import { useStore } from "@/js/lib/store";
 
@@ -16,6 +17,21 @@ export default function Actions() {
         orbitUp: button(async () => await orbit("up")),
         orbitDown: button(async () => await orbit("down")),
         resetCameraTarget: button(() => resetCameraTarget()),
+    });
+    useHotkeys("esc", () => {
+        resetCurrentBoxUuid();
+    });
+    useHotkeys("up", async () => {
+        await orbit("up");
+    });
+    useHotkeys("down", async () => {
+        await orbit("down");
+    });
+    useHotkeys("right", async () => {
+        await orbit("right");
+    });
+    useHotkeys("left", async () => {
+        await orbit("left");
     });
 
     return null;
