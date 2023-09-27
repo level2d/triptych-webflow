cc764e9c627ec3f664aef93cc8a158a68aee74fa
Mario Perez <mario.perez415@gmail.com>
2023-09-27 13:03:09 -0400

    Add shader to methods model

--- js/components/3D/Models/MethodsModel.jsx
+++ js/components/3D/Models/MethodsModel.jsx
@@ -7,9 +7,14 @@ import { useGLTF, useAnimations } from "@react-three/drei";
 import * as THREE from "three";
 
 import { GLB_ASSET_URLS } from "@/js/core/constants";
+import { GrainMaterialYellow, GrainMaterialYellowDark } from "../Materials";
 
 export default function MethodsModel(props) {
     const [mounted, setMounted] = useState(false);
+    const [boundingBox, setBoundingBox] = useState({
+        min: new THREE.Vector3(0, 0, 0),
+        max: new THREE.Vector3(1, 1, 1),
+    });
     const group = useRef();
     const { nodes, animations } = useGLTF(GLB_ASSET_URLS.Methods);
     const { actions, names } = useAnimations(animations, group);
@@ -38,9 +43,17 @@ export default function MethodsModel(props) {
         });
     }, [mounted, actions, names]);
 
+    // setup uniforms
+    useEffect(() => {
+        const bb = new THREE.Box3();
+        bb.setFromObject(group.current);
+        setBoundingBox(bb);
+    }, []);
+
     useEffect(() => {
         setMounted(true);
     }, []);
+
     return (
         <group ref={group} {...props} dispose={null} onClick={handleClick}>
             <group name="Scene">
@@ -52,46 +65,70 @@ export default function MethodsModel(props) {
                                 castShadow
                                 receiveShadow
                                 geometry={nodes.Cylinder002.geometry}
-                                material={nodes.Cylinder002.material}
-                            />
+                                // material={nodes.Cylinder002.material}
+                            >
+                                <GrainMaterialYellowDark
+                                    boundingBox={boundingBox}
+                                />
+                            </mesh>
                             <mesh
                                 name="Cylinder002_1"
                                 castShadow
                                 receiveShadow
                                 geometry={nodes.Cylinder002_1.geometry}
-                                material={nodes.Cylinder002_1.material}
-                            />
+                                // material={nodes.Cylinder002_1.material}
+                            >
+                                <GrainMaterialYellow
+                                    boundingBox={boundingBox}
+                                />
+                            </mesh>
                             <mesh
                                 name="gear_01"
                                 castShadow
                                 receiveShadow
                                 geometry={nodes.gear_01.geometry}
-                                material={nodes.gear_01.material}
+                                // material={nodes.gear_01.material}
                                 position={[-0.264, -0.152, 0]}
-                            />
+                            >
+                                <GrainMaterialYellow
+                                    boundingBox={boundingBox}
+                                />
+                            </mesh>
                             <mesh
                                 name="gear_02"
                                 castShadow
                                 receiveShadow
                                 geometry={nodes.gear_02.geometry}
-                                material={nodes.gear_02.material}
+                                // material={nodes.gear_02.material}
                                 position={[0, 0.305, 0]}
-                            />
+                            >
+                                <GrainMaterialYellow
+                                    boundingBox={boundingBox}
+                                />
+                            </mesh>
                             <mesh
                                 name="gear_03"
                                 castShadow
                                 receiveShadow
                                 geometry={nodes.gear_03.geometry}
-                                material={nodes.gear_03.material}
+                                // material={nodes.gear_03.material}
                                 position={[0.264, -0.152, 0]}
-                            />
+                            >
+                                <GrainMaterialYellow
+                                    boundingBox={boundingBox}
+                                />
+                            </mesh>
                             <mesh
                                 name="gear_04"
                                 castShadow
                                 receiveShadow
                                 geometry={nodes.gear_04.geometry}
-                                material={nodes.gear_04.material}
-                            />
+                                // material={nodes.gear_04.material}
+                            >
+                                <GrainMaterialYellow
+                                    boundingBox={boundingBox}
+                                />
+                            </mesh>
                         </group>
                     </group>
                 </group>
