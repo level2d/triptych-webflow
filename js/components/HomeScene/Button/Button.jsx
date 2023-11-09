235026f25e2f28abfe7517e391ad307b93ef6430
Mario Perez <mario.perez415@gmail.com>
2023-11-09 15:44:24 -0500

    Synchronize button styles with webflow buttons

--- js/components/HomeScene/Button/Button.jsx
+++ js/components/HomeScene/Button/Button.jsx
@@ -4,20 +4,23 @@ import styles from "./Button.module.scss";
 import { BUTTON_THEMES } from "./constants";
 
 export default function Button({
+    as = "button",
     theme = BUTTON_THEMES.dark,
     children,
     ...props
 }) {
+    let Component = as;
     const themeClassname = useMemo(() => {
         return styles[`button--${theme}`];
     }, [theme]);
 
     return (
-        <button
+        <Component
             className={cx("caption-regular", styles.button, themeClassname)}
             {...props}
         >
-            {children}
-        </button>
+            <span className={styles.buttonText}>{children}</span>
+            <span className={styles.buttonOutline}></span>
+        </Component>
     );
 }
---
+++ js/components/HomeScene/Button2/index.jsx
@@ -0,0 +1,3 @@
+export { default } from "./Button2";
+// eslint-disable-next-line
+export * from "./constants";
