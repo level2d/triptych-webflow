12b68ae1976aad0039f208fb406310fc92c33beb
Mario Perez <mario.perez415@gmail.com>
2023-11-10 12:03:03 -0500

    Prettier format

--- js/components/BackgroundFx/RippleScene/BgScene.jsx
+++ js/components/BackgroundFx/RippleScene/BgScene.jsx
@@ -11,29 +11,29 @@ import { extend, useThree } from "@react-three/fiber";
 // import imgUrl from '@/assets/img/Dustin.png'
 
 const BgScene = forwardRef((props, ref) => {
-  const { viewport } = useThree();
-  // const texture = useTexture(imgUrl);
+    const { viewport } = useThree();
+    // const texture = useTexture(imgUrl);
 
-  // const BgShaderMaterial = shaderMaterial(
-  //     // Uniform
-  //     {
-  //         time: { value: 0 },
-  //         uDisplacement: { value: null },
-  //         uTexture: { value: null },
-  //         resolution: { value: new THREE.Vector4() },
-  //     },
-  //     vertex,
-  //     fragment,
-  // );
+    // const BgShaderMaterial = shaderMaterial(
+    //     // Uniform
+    //     {
+    //         time: { value: 0 },
+    //         uDisplacement: { value: null },
+    //         uTexture: { value: null },
+    //         resolution: { value: new THREE.Vector4() },
+    //     },
+    //     vertex,
+    //     fragment,
+    // );
 
-  // extend({ BgShaderMaterial });
+    // extend({ BgShaderMaterial });
 
-  return (
-      <>
-          <mesh ref={ref}>
-              <planeGeometry args={[viewport.width, viewport.height]} />
-              <RippleMaterial opacity={1} uDisplacement={null} />
-              {/* <bgShaderMaterial
+    return (
+        <>
+            <mesh ref={ref}>
+                <planeGeometry args={[viewport.width, viewport.height]} />
+                <RippleMaterial opacity={1} uDisplacement={null} />
+                {/* <bgShaderMaterial
                   time={0}
                   uDisplacement={null}
                   uTexture={texture}
@@ -41,12 +41,11 @@ const BgScene = forwardRef((props, ref) => {
                       new THREE.Vector2(viewport.width, viewport.height)
                   }
               /> */}
-          </mesh>
-      </>
-  );
+            </mesh>
+        </>
+    );
 });
 
 BgScene.displayName = "BgScene";
-
 
 export default BgScene;
--- js/components/BackgroundFx/Scene.jsx
+++ js/components/BackgroundFx/Scene.jsx
@@ -1,48 +1,45 @@
 import { Perf } from "r3f-perf";
 import { debug } from "@/js/core/constants";
-import { PerspectiveCamera} from "@react-three/drei";
-import { useFrame} from "@react-three/fiber";
+import { PerspectiveCamera } from "@react-three/drei";
+import { useFrame } from "@react-three/fiber";
 import { useRef } from "react";
 import { RippleMaterial } from "../3D/Materials";
 
 export default function Scene() {
+    const planeRef = useRef(null);
 
-  const planeRef = useRef(null);
-   
-    useFrame(({viewport, camera})=>{
-      const { height, width } = viewport.getCurrentViewport(
-        camera,
-        [0, 0, 0]
-    );
-        if(planeRef.current){
-          planeRef.current.scale = [width, height, 1];
+    useFrame(({ viewport, camera }) => {
+        const { height, width } = viewport.getCurrentViewport(
+            camera,
+            [0, 0, 0],
+        );
+        if (planeRef.current) {
+            planeRef.current.scale = [width, height, 1];
         }
     });
 
     return (
         <>
-                {/* debug */}
-                {debug && <Perf position="top-left" />}
-                {debug && <axesHelper args={[5]} />}
+            {/* debug */}
+            {debug && <Perf position="top-left" />}
+            {debug && <axesHelper args={[5]} />}
 
-                {/* camera, controls, etc... */}
-                <PerspectiveCamera makeDefault position={[0,0,1]} />
-                {/* <OrbitControls makeDefault/> */}
+            {/* camera, controls, etc... */}
+            <PerspectiveCamera makeDefault position={[0, 0, 1]} />
+            {/* <OrbitControls makeDefault/> */}
 
-                {/* meshes */}
-                <mesh
-                    position={[0, 0, 0]}
-                >
-                    <planeGeometry args={[2, 2]} attach={"geometry"} />
-                    {/* <meshStandardMaterial
+            {/* meshes */}
+            <mesh position={[0, 0, 0]}>
+                <planeGeometry args={[2, 2]} attach={"geometry"} />
+                {/* <meshStandardMaterial
                         color={"red"}
                         attach={"material"}
                         side={THREE.DoubleSide}
                     /> */}
-                    <RippleMaterial opacity={1}/>
-                </mesh>
+                <RippleMaterial opacity={1} />
+            </mesh>
 
-                <ambientLight intensity={1} />
+            <ambientLight intensity={1} />
         </>
     );
 }
