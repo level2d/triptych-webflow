e032d5b80bd3ada57a1e6f2397e8aa4cefa1c243
Mario Perez <mario.perez415@gmail.com>
2023-12-07 11:10:16 -0500

    Remove side effects

--- vite.config.js
+++ vite.config.js
@@ -44,6 +44,9 @@ export default defineConfig((command, mode) => {
                     entryFileNames: "main.js",
                     esModule: false,
                     compact: true,
+                    treeshake: {
+                        moduleSideEffects: false,
+                    },
                     globals: {
                         // webflow sites have global jQuery
                         jquery: "window.jQuery",
