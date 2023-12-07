3689bdafcf10625bfd1e72180c1fc095d1b50b38
Mario Perez <mario.perez415@gmail.com>
2023-12-07 10:09:11 -0500

    Remove barrel files from HomeScene

--- js/components/HomeScene/Nav/index.jsx
+++ js/components/HomeScene/Nav/index.jsx
@@ -1,1 +1,109 @@
-export { default } from "./Nav";
+import styles from "./Nav.module.scss";
+
+import { useLayoutEffect, useMemo, useRef } from "react";
+import cx from "classnames";
+
+import gsap from "@/js/lib/gsap";
+import { useStore } from "@/js/lib/store";
+import Actions from "./Actions";
+import NavButton from "../NavButton";
+
+const NavArrows = ({ visible }) => {
+    const el = useRef(null);
+    const orbit = useStore((state) => state.orbit);
+    const disabled = useStore((state) => state.isOrbiting);
+    useLayoutEffect(() => {
+        const target = el.current;
+        if (!target) {
+            return;
+        }
+        gsap.fromTo(
+            target,
+            {
+                opacity: visible ? 0 : 1,
+                y: visible ? 50 : 0,
+                zIndex: visible ? -1 : 1,
+                visibility: visible ? "hidden" : "visible",
+            },
+            {
+                opacity: visible ? 1 : 0,
+                y: visible ? 0 : -50,
+                zIndex: visible ? 1 : -1,
+                visibility: visible ? "visible" : "hidden",
+                duration: 0.2,
+            },
+        );
+        return () => {
+            gsap.killTweensOf(target);
+        };
+    }, [visible]);
+    return (
+        <div className={styles.arrows} ref={el}>
+            <div className={styles.buttonWrapper}>
+                <NavButton
+                    direction="left"
+                    onClick={() => {
+                        orbit("left");
+                    }}
+                    disabled={disabled}
+                />
+                {/* <div className={styles.middleButtons}>
+                    <NavButton
+                        direction="up"
+                        onClick={() => {
+                            orbit("up");
+                        }}
+                        disabled={disabled}
+                    />
+                    <NavButton
+                        direction="down"
+                        onClick={() => {
+                            orbit("down");
+                        }}
+                        disabled={disabled}
+                    />
+                </div> */}
+                <NavButton
+                    direction="right"
+                    onClick={() => {
+                        orbit("right");
+                    }}
+                    disabled={disabled}
+                />
+            </div>
+            <div className={styles.textWrapper}>
+                <span className={cx("hpe-nav-text", styles.text)}>
+                    Use arrows to explore
+                </span>
+            </div>
+        </div>
+    );
+};
+
+const NavUi = () => {
+    const interactable = useStore((state) => state.interactable);
+    const currentBoxUuid = useStore((state) => state.currentBoxUuid);
+
+    const isNavArrowsVisible = useMemo(() => {
+        return currentBoxUuid === null;
+    }, [currentBoxUuid]);
+
+    if (!interactable) {
+        return null;
+    }
+
+    return (
+        <>
+            <NavArrows visible={isNavArrowsVisible} />
+        </>
+    );
+};
+
+export default function Nav() {
+    return (
+        <>
+            <NavUi />
+            <Actions />
+        </>
+    );
+}
--- js/components/HomeScene/index.jsx
+++ js/components/HomeScene/index.jsx
@@ -1,1 +1,61 @@
-export { default } from "./HomeScene";
+import styles from "./HomeScene.module.scss";
+
+import { Suspense, lazy } from "react";
+import { Canvas } from "@react-three/fiber";
+import { Leva } from "leva";
+
+import { debug } from "@/js/core/constants";
+import { useStore } from "@/js/lib/store";
+import Loading from "./Loading";
+import Nav from "./Nav";
+import CurrentItemUi from "./CurrentItemUi";
+import useQueryVariable from "@/js/hooks/useQueryVariable";
+
+import "../3D/Shaders";
+
+const Scene = lazy(() => {
+    return Promise.all([
+        import("./Scene"),
+        new Promise((resolve) => setTimeout(resolve, debug ? 0 : 1 * 500)),
+    ]).then(([moduleExports]) => moduleExports);
+});
+const DebugModelScene = lazy(() => import("./DebugModelScene"));
+
+export default function HomeScene() {
+    const setGetR3fStore = useStore((state) => state.setGetR3fStore);
+    const wrapperProps = {};
+    const debugModel = useQueryVariable("debugModel");
+    if (debug) {
+        wrapperProps["data-lenis-prevent"] = true;
+    }
+    return (
+        <div className={styles.homeScene}>
+            <div className={styles.homeSceneCanvasWrapper} {...wrapperProps}>
+                <Canvas
+                    shadows
+                    onCreated={({ get, gl }) => {
+                        // Pass r3f store getter to our local store
+                        setGetR3fStore(get);
+                        gl.setClearAlpha(0);
+                    }}
+                    gl={{
+                        antialias: true,
+                        alpha: true,
+                    }}
+                >
+                    <Suspense fallback={null}>
+                        {debug && debugModel ? (
+                            <DebugModelScene model={debugModel} />
+                        ) : (
+                            <Scene />
+                        )}
+                    </Suspense>
+                </Canvas>
+            </div>
+            <Loading />
+            {!debugModel && <Nav />}
+            {!debugModel && <CurrentItemUi />}
+            <Leva hidden={!debug} collapsed oneLineLabels />
+        </div>
+    );
+}
