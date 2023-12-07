afb4dd77aad3681a1ccc9361a251c15a91456607
Mario Perez <mario.perez415@gmail.com>
2023-12-07 10:25:05 -0500

    Remove modules barrel file

--- js/modules/index.js
+++
@@ -1,9 +0,0 @@
-export { default as FadeInText } from "./FadeInText";
-export { default as FancyImage } from "./FancyImage";
-export { default as HomeExperience } from "./HomeExperience";
-export { default as BackgroundFx } from "./BackgroundFx";
-export { default as KeepScrolling } from "./KeepScrolling";
-export { default as Loader } from "./Loader";
-export { default as PopQuote } from "./PopQuote";
-export { default as ScrambleText } from "./ScrambleText";
-export * as test from "./test";
