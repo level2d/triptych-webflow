d8e3519fa1a988c8361df125c61fa85e4336eb4c
Mario Perez <mario.perez415@gmail.com>
2024-02-01 14:37:16 -0500

    Render sound indictor to a module target

--- js/modules/SoundIndicator.js
+++ js/modules/SoundIndicator.js
@@ -4,24 +4,17 @@ import App from "@/js/App";
 
 export default class SoundIndicator {
     enabled = false;
-    renderTarget = null;
     $target = null;
     app = null;
     constructor() {
         this.app = new App();
-        this.$target = this.app.core.dom.document;
+        this.$target = this.app.core.dom.soundIndicator[0];
     }
 
-    bindListeners() {}
-
     init() {
-        if (this.$target) {
+        if (this.$target[0]) {
             this.enabled = true;
-            this.bindListeners();
-            this.renderTarget = document.createElement("div");
-            this.renderTarget.id = "audio-toggle";
-            this.$target.prepend(this.renderTarget);
-            renderToDOMElement(this.renderTarget, SoundIndicatorComponent);
+            renderToDOMElement(this.$target[0], SoundIndicatorComponent);
             console.log("Module: Audio Toggle: init");
         }
     }
